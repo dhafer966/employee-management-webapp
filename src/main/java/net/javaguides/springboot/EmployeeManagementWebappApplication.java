e998190bfe968bfdf75c8589aaba0146f6abdd1a
dhafer966 <dhaferrazwen@gmail.com>
2022-02-27 15:17:35 +0100

    third commit

--- src/main/java/net/javaguides/springboot/EmployeeManagementWebappApplication.java
+++ src/main/java/net/javaguides/springboot/EmployeeManagementWebappApplication.java
@@ -2,9 +2,11 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeManagementWebappApplication {
+public class EmployeeManagementWebappApplication extends SpringBootServletInitializer{
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementWebappApplication.class, args);
